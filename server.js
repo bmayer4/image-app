7a56e9dc6b93e7e80ce41959005e5c62f91fec7a
Brett Mayer <bmayer55@yahoo.com>
2018-10-03 11:17:34 -0400

    image

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ app.use('/images', express.static(path.join(__dirname, 'images')));  //any req t
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.use('/images', express.static(path.join(__dirname, 'images')));
 
     app.get('*' , (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
