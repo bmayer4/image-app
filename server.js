9ddb23922ee7992574eea031c70dd066f7d188f1
Brett Mayer <bmayer55@yahoo.com>
2018-10-03 08:05:06 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ app.use('/images', express.static(path.join(__dirname, 'images')));  //any req t
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    app.get('*' , (req, res => {
+    app.get('*' , (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    }))
+    });
 }
 
 
