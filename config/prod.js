94d10ab5b40e58799ddf43260e051a74ad20f57c
Brett Mayer <bmayer55@yahoo.com>
2018-10-04 00:10:34 -0400

    added keys to heroku

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGOURI,
-    secret: process.env.SECRET_OR_KEY
+    secret: process.env.SECRET_OR_KEY,
+    API_Key: process.env.API_Key,
+    API_Secret: process.env.API_Secret,
+    cloudName: process.env.cloudName
 }
