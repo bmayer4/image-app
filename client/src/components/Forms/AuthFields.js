cbe10d9b862f770466e9e0c83405562971859bb1
Brett Mayer <bmayer55@yahoo.com>
2019-04-21 16:20:57 -0400

    added confirm password

--- client/src/components/Forms/AuthFields.js
+++ client/src/components/Forms/AuthFields.js
@@ -2,7 +2,8 @@ export const JOINFIELDS = [
     { label: 'First Name', name: 'firstName', type: 'text' },
     { label: 'Last Name', name: 'lastName', type: 'text' },
     { label: 'Email', name: 'email', type: 'text' },
-    { label: 'Password', name: 'password', type: 'password' }
+    { label: 'Password', name: 'password', type: 'password' },
+    { label: 'Confirm Password', name: 'confirmPassword', type: 'password' }
 ];
 
 export const LOGINFIELDS = [
--- client/src/components/Forms/validate.js
+++ client/src/components/Forms/validate.js
@@ -12,15 +12,17 @@ export const joinValidate = (values) => {
     });
 
     if (!values.password) {
-        errors.password = 'Password required'
-      } else if (values.password.length < 4 || values.password.length > 12) {
-        errors.password = 'Password must be between 4 and 12 characters'
-      }
+        errors.password = 'Password required';
+    } else if (values.password.length < 4 || values.password.length > 12) {
+        errors.password = 'Password must be between 4 and 12 characters';
+    } else if (values.password != values.confirmPassword) {
+        errors.confirmPassword = 'Passwords must match';
+    }
   
     if (!values.email) {
-      errors.email = 'Email required'
+      errors.email = 'Email required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email'
+      errors.email = 'Invalid email';
     }
   
     return errors
